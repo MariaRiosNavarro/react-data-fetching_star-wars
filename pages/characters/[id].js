97d1b1427794d08113423a4b888fbe3e5ebb6ff7
Maria Rios Navarro <propscode254@gmail.com>
2023-06-08 20:59:00 +0200

    add fetcher in _app and change route to [id]

---
+++ pages/characters/[id].js
@@ -0,0 +1,38 @@
+import { useRouter } from "next/router";
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+
+export default function Character() {
+  //access router object to get ID from query parameters
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, error } = useSWR(`https://swapi.dev/api/people/${id}`, fetcher);
+
+  if (error) {
+    return <div>Error: error.message</div>;
+  }
+
+  if (!data) {
+    return <div>loadConfig... </div>;
+  }
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
+
+
+// //  const { data, error, isLoading } = useSWR(
+//   `https://swapi.dev/api/people/${id}`
+//   );
